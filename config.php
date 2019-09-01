83bbbc8f9a0af6b76abd6ff3a2c72dd8fae7f1ff
zwssd1980 <zwssd1980@163.com>
2019-09-01 21:29:36 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+// 框架版本
+define('VERSION','0.0.1');
+
+/* 应用环境
+ *
+ *  dev
+ *  test
+ *  pro
+ *
+ */
+define('ENV', 'dev');
+
+// 系统目录
+define('SYSTEM_PATH','system'.DIRECTORY_SEPARATOR);
+
+// 应用目录
+define('APP_PATH','app'.DIRECTORY_SEPARATOR);
---
+++ system/ShenNong.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace shennong;
+
+// 框架根目录
+defined('SN_PATH') or define('SN_PATH', __DIR__);
+
+/*
+** 框架核心
+*/
+class ShenNong{
+
+    public function __construct($config)
+    {
+        $this->config = '';
+    }
+}
