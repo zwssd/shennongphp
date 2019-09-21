8e61cd3a3317a4ca55d9d23d34746eb6920194ba
zhangwei <zwssd1980@163.com>
2019-09-21 23:56:17 +0800

    增加了生成的cache目录

--- app/model/blog/default.php
+++ app/model/blog/default.php
@@ -1,12 +1,12 @@
 <?php
 class blogdefaultModel extends Model {
     public function getUser($id){
-        //$user_data = $this->cache->get('blog.default.user');
+        $user_data = $this->cache->get('blog.default.user');
         if(!$user_data){
             $results = $this->db->query('select * from sn_user','object');
             $user_data = $results->query;
 
-            //$this->cache->set('blog.default.user',$user_data);
+            $this->cache->set('blog.default.user',$user_data);
         }
         return $user_data;
     }
